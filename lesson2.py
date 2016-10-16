2f44be561aae032d5627a2e0811b034fa0a1dc4a
ChenWeiMing <will19940625@gmail.com>
2016-10-17 00:05:40 +0800

    Lesson 2 create
    if-else 判斷式

---
+++ lesson2.py
@@ -0,0 +1,35 @@
+# 判斷式
+# if-else
+
+true_value = 'true'
+false_value = 'false'
+
+# boolean_true = true is not defined
+# boolean_false = false is not defined
+# if(boolean_true):
+#     print(true_value)
+# else:
+#     print(false_value)
+# result error
+# true , false 不能作為特殊字串或是boolean值使用
+
+boolean_true = "true"
+boolean_false = "false"
+
+if(boolean_true):
+    print(true_value)
+else:
+    print(false_value)
+# result = true_value = true
+
+if(true_value):
+    print(boolean_true)
+else:
+    print(boolean_false)
+# result = boolean_true = true
+
+if boolean_true:
+    print(true_value)
+else:
+    print(false_value)
+# result = true_value = true
