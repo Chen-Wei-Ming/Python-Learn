d68196206e35a2b294898183d476544452643906
ChenWeiMing <will19940625@gmail.com>
2016-10-16 23:43:03 +0800

    Lesson 1 create
    變數建立

---
+++ lesson1.py
@@ -0,0 +1,26 @@
+# 變數建立
+# create a var
+
+# var is a integer
+var = 1
+print(var)
+
+# var is a float
+var = 1.0
+print(var)
+
+# var is a character
+var = '1'
+print(var)
+
+# var is a String
+var = 'string'
+print(var)
+
+# var is a Array
+var = ['array1' , 'array2' , 'array3']
+print(var)
+
+# print item which in var array
+for item in var :
+    print(item)
